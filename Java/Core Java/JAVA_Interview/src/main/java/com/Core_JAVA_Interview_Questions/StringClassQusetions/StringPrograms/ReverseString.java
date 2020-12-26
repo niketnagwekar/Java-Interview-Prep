c294f284b00bbb96d65a97590a35c22152846717
Niket Nagwekar <nagwekarniket23@gmail.com>
2020-12-26 17:09:45 +0530

    Update ReverseString.java

--- Java/Core Java/JAVA_Interview/src/main/java/com/Core_JAVA_Interview_Questions/StringClassQusetions/StringPrograms/ReverseString.java
+++ Java/Core Java/JAVA_Interview/src/main/java/com/Core_JAVA_Interview_Questions/StringClassQusetions/StringPrograms/ReverseString.java
@@ -14,6 +14,44 @@ public class ReverseString {
 		return recursiveMethod(str.substring(1)) + str.charAt(0);
 	}
 
+	// reverse a string preserving spaces
+	static void reverseString(String inputString) {
+		// Converting inputString to char array 'inputStringArray'
+		char[] inputStringArray = inputString.toCharArray();
+
+		// Defining a new char array 'resultArray' with same size as inputStringArray
+		char[] resultArray = new char[inputStringArray.length];
+
+		// First for loop :
+		// For every space in the 'inputStringArray',
+		// we insert spaces in the 'resultArray' at the corresponding positions
+		for (int i = 0; i < inputStringArray.length; i++) {
+			if (inputStringArray[i] == ' ') {
+				resultArray[i] = ' ';
+			}
+		}
+
+		// Initializing 'j' with length of resultArray
+		int j = resultArray.length - 1;
+
+		// Second for loop :
+		// we copy every non-space character of inputStringArray
+		// from first to last at 'j' position of resultArray
+		for (int i = 0; i < inputStringArray.length; i++) {
+			if (inputStringArray[i] != ' ') {
+				// If resultArray already has space at index j then decrementing 'j'
+				if (resultArray[j] == ' ') {
+					j--;
+				}
+
+				resultArray[j] = inputStringArray[i];
+
+				j--;
+			}
+		}
+		System.out.println(inputString + " ---> " + String.valueOf(resultArray));
+	}
+
 	public static void main(String[] args) {
 
 		// 1) Using StringBuffer class:
@@ -28,6 +66,9 @@ public class ReverseString {
 		}
 		System.out.println();
 		System.out.println(recursiveMethod(str));
+
+		// using reverse a string preserving spaces
+		reverseString("I Am Not String");
 
 	}
 
