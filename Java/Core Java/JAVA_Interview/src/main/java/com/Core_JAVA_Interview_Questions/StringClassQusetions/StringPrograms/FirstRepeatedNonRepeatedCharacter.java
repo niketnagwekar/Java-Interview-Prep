01fbe70edfd002a120d79a50d2f8649299df595f
Niket Nagwekar <nagwekarniket23@gmail.com>
2020-12-26 16:48:44 +0530

    How To Find First Repeated And Non-Repeated Character In A String?

---
+++ Java/Core Java/JAVA_Interview/src/main/java/com/Core_JAVA_Interview_Questions/StringClassQusetions/StringPrograms/FirstRepeatedNonRepeatedCharacter.java
@@ -0,0 +1,35 @@
+package com.Core_JAVA_Interview_Questions.StringClassQusetions.StringPrograms;
+
+import java.util.HashMap;
+
+public class FirstRepeatedNonRepeatedCharacter {
+
+	static void firstRepeatedNonRepeatedChar(String inputString) {
+		HashMap<Character, Integer> charCountMap = EachCharCountInString.characterCount(inputString);
+
+		// Converting inputString to char array
+		char[] strArray = inputString.toCharArray();
+		
+		// Checking for first non-repeated character
+		for (char ch : strArray) {
+			if(charCountMap.get(ch).equals(1)) {
+				System.out.println("First Non-Repeated Character In '"+inputString+"' is '"+ch+"'");
+				break;
+			}
+		}
+		
+		// Checking for first repeated character
+		for (char ch : strArray) {
+			if(charCountMap.get(ch) > 1) {
+				System.out.println("First Repeated Character In '"+inputString+"' is '"+ch+"'");
+				break;
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		firstRepeatedNonRepeatedChar("JavaConceptOfTheDay".toLowerCase().trim());
+	}
+
+}
