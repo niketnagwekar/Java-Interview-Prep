5885fa497efa74c5f7fb95b7090e87f5a5e15177
Niket Nagwekar <nagwekarniket23@gmail.com>
2021-01-05 14:33:36 +0530

    ExecutorService

--- Java/Core Java/JAVA_Interview/src/main/java/com/Core_JAVA_Interview_Questions/ConcurrencyQuestions/ExecutorAndExecutorService.java
+++ Java/Core Java/JAVA_Interview/src/main/java/com/Core_JAVA_Interview_Questions/ConcurrencyQuestions/ExecutorAndExecutorService.java
@@ -1,32 +1,57 @@
 package com.Core_JAVA_Interview_Questions.ConcurrencyQuestions;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 public class ExecutorAndExecutorService {
 
-	public static void main(String[] args) {
-
-	}
-
-}
-
-class MyRunnable implements Runnable {
-	 
-    int taskNumber;
- 
-    MyRunnable(int taskNumber) {
-           this.taskNumber = taskNumber;
+	public static void main(String[] args) 
+    {
+        //Demo task
+        Runnable runnableTask = () -> {
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+                System.out.println("Current time :: " + LocalDateTime.now());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+         
+        //Executor service instance
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+         
+        //1. execute task using execute() method
+        executor.execute(runnableTask);
+         
+        //2. execute task using submit() method
+        Future<String> result = executor.submit(runnableTask, "DONE");
+         
+        while(result.isDone() == false) 
+        {
+            try
+            {
+                System.out.println("The method return value : " + result.get());
+                break;
+            } 
+            catch (InterruptedException | ExecutionException e) 
+            {
+                e.printStackTrace();
+            }
+             
+            //Sleep for 1 second
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+         
+        //Shut down the executor service
+        executor.shutdownNow();
     }
- 
-    @Override
-    public void run() {
- 
-           System.out.println(Thread.currentThread().getName()
-                        + " executing task no " + taskNumber);
- 
-           try {
-                  Thread.sleep(1000);
-           } catch (InterruptedException e) {
-                  e.printStackTrace();
-           }
- 
-    }
+
 }
